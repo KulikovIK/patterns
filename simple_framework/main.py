42c78aea70ab39269e742b2117b388f0c7c77b4d
Kulikov I.K <ilyatrg@gmail.com>
2022-12-11 22:56:11 +0500

    lesson_2 - GET and POST requests handlers, core(view, templator) and utils(simplelogger) added

--- simple_framework/main.py
+++ simple_framework/main.py
@@ -1,3 +1,9 @@
+from quopri import decodestring
+
+from simple_framework.request_handlers import GetRequests, PostRequests
+from simple_framework.utils.simple_loger import print_log
+
+
 class PageNotFound404:
     def __call__(self, request) -> [str, str]:
         return '404 WHAT', '404 Page not found'
@@ -12,11 +18,26 @@ class Framework:
 
     def __call__(self, environ, start_response):
         # получаем адрес, по которому выполнен переход
-        path = environ['PATH_INFO']
+        path = environ.get('PATH_INFO')
 
         # добавление закрывающего слеша
         if not path.endswith('/'):
             path = f'{path}/'
+
+        request = {}
+        # получение параметров запроса
+        method = environ.get('REQUEST_METHOD')
+        request['method'] = method
+
+        if method == 'GET':
+            request_params = GetRequests().get_request_params(environ)
+            request['request_params'] = Framework.decode_value(request_params)
+
+        if method == 'POST':
+            request_params = PostRequests().get_request_params(environ)
+            request['request_params'] = Framework.decode_value(request_params)
+
+        print_log(method, request.get("request_params"))
 
         # находим нужный контроллер
         # отработка паттерна page controller
@@ -24,7 +45,6 @@ class Framework:
             view = self.routers_list[path]
         else:
             view = PageNotFound404()
-        request = {}
 
         # наполняем словарь request элементами
         # этот словарь получат все контроллеры
@@ -37,3 +57,13 @@ class Framework:
         code, body = view(request)
         start_response(code, [('Content-Type', 'text/html')])
         return [body.encode('utf-8')]
+
+    @staticmethod
+    def decode_value(data: dict) -> dict:
+        decoded_data = {}
+        for key, value in data.items():
+            value = bytes(value.replace('%', '=').replace('+', ' '), 'utf-8')
+            value = decodestring(value).decode('utf-8')
+            decoded_data[key] = value
+
+        return decoded_data
